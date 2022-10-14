3d5da2ff15a5190d4c909f2dc1ed974132483ed6
Zach Ruiz <powersbooth10@gmail.com>
2022-10-14 18:43:16 -0500

    commit tree 2bd9e78ebd76b3bdaf24bf06fd8db823592b2f5d

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
 
-def countdown
+def countdown(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        sleep(1)
+        number -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
